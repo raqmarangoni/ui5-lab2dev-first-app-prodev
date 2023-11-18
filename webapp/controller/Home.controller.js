3be829751115849ec87875134fb16cbcf16bd9b2
raqmarangoni <rmarangonisan@gmail.com>
2023-11-18 14:52:04 -0300

    feat: criar lista de produtos

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,20 +1,56 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast','sap/ui/core/mvc/Controller"
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Home", {
             onInit: function () {
 
+                const list = [
+                    {
+                        name: "Banana",
+                        color: "Yellow",
+                        quantity: 6
+                    },
+                    {
+                        name: "Apple",
+                        color: "Red",
+                        quantity: 3
+                    },
+                    {
+                        name: "Blueberry",
+                        color: "Blue",
+                        quantity: 5
+                    },
+                    {
+                        name: "Orange",
+                        color: "Orange",
+                        quantity: 1
+                    },
+                    {
+                        name: "Strawberry",
+                        color: "red",
+                        quantity: 9
+                    },
+                    {
+                        name: "Kiwi",
+                        color: "Green",
+                        quantity: 1
+                    },
+                    
+                ]
+
+                var oModel = new JSONModel(list);
+                this.getView().setModel(oModel, "listModel");
+
             },
-            onHelloButtonPress: function (test){
-                console.log("Hello");
-                console.log(test)
+            onHello: function(){
+                console.log("Hello World")
             }
         });
     });
